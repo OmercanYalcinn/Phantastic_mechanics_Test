988033f7776253161311b6618dbbd2c52be3b914
Ömercan Yalçın <111580457+OmercanYalcinn@users.noreply.github.com>
2023-09-18 20:07:04 +0300

    Add files via upload

---
+++ ScoreSystem.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public TextMeshPro scoreText;
+    private CoinSystem _coinSystem;
+
+    private void Start()
+    {
+        _coinSystem = FindObjectOfType<CoinSystem>();
+    }
+
+    private void Update()
+    {
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        if (_coinSystem != null)
+        {
+            // coinCount is coming from CoinsSystem class
+            scoreText.text = "Score: " + _coinSystem.coinCount;
+        }
+    }
+}
